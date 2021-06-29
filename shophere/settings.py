418cfa2f7a5b475353cb2aec6d53f4d12511f5ec
faizan1166 <71093180+faizan1166@users.noreply.github.com>
2021-06-29 03:11:22 -0400

    Update settings.py

--- shophere/settings.py
+++ shophere/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
